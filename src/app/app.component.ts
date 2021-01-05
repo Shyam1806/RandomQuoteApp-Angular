ee948afadc23fb4028a4451d398933dea9ce10b1
shyam <you@example.com>
2021-01-05 21:22:39 +0530

    ch(deploy) :Deploy Base application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  {
   title = 'RandomQuoteApp';
-}
+  myimage :string ="assets/images/Quote1.jpg";
+
+ 
+
+  url ='https://api.quotable.io/random';
+  constructor (public http: HttpClient){}
+    getdataz(){
+      fetch(this.url)
+    .then(function(data){
+      
+      return data.json();
+     
+    })
+    .then(function(data){
+      
+      document.getElementById("randomans").innerHTML = data.content;
+      // document.getElementById("authorz").innerHTML = data.author;
+    })
+    .catch(function err(){
+      alert("Try Again ");
+    })    
+  }
+  }
+ 
+    
+  
+    
+
+
